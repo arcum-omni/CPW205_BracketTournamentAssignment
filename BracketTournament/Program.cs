de8b85f972b23b7731f675807e6263801ae3b1b8
arcum-omni <travis.l.eiler@gmail.com>
2020-07-16 11:27:11 -0700

    Refactored out magic numbers

--- BracketTournament/Program.cs
+++ BracketTournament/Program.cs
@@ -18,7 +18,7 @@ namespace BracketTournament
     {
         static void Main(string[] args)
         {
-            //  ODO: Psuedo code map for assignment
+            //  TODO: Psuedo code map for assignment
             //  Read Tournaments bracket data from file
             //  Validate number of participants
             //  Assign each line/player a random unique bracket id
@@ -37,25 +37,25 @@ namespace BracketTournament
             //  Compare finalist
             //  Winner of 0/1 gets first & $25, loser gets 2nd & $10
 
-            // Random Number Generator
-            var rand = new Random();
 
             // Declare variables
             int numParticipants;
             int participantsNeeded;
 
             // File Path & Name
-            String filePath =  @"D:\Documents\CPW205_OODA\HW01\BracketTournament\BracketTournament\ReferenceMaterials\";
-            String fileName = "tournament.txt";
+            string filePath = @"D:\Documents\CPW205_OODA\HW01\BracketTournament\BracketTournament\ReferenceMaterials\";
+            string fileName = "tournament.txt";
+            //string fileName = "largeTournament.txt";
 
             // Create an array of participants from the text file
-            string[] txtArray = File.ReadAllLines(filePath+fileName);
+            string[] txtArray = File.ReadAllLines(filePath + fileName);
             
             // Determine Number of Participants
             numParticipants = txtArray.Length;
 
             // Display list of participants, in the order they signed up.
-            Console.WriteLine("Bracket Entry Form: " + fileName);
+            Console.Write("Bracket Entry Sign Up Form: ");
+            Console.WriteLine("(" + fileName + ")");
             foreach (string line in txtArray)
             {
                 // Use a tab to indent each line of the file.
@@ -76,37 +76,51 @@ namespace BracketTournament
                 {
                     singularPlural = "bracket";
                 }
-                Console.WriteLine("We have " + numberOfBrackets + " " + singularPlural + " in the brackets tournament");
+                Console.WriteLine("There are enough entries to form " + numberOfBrackets + " " + singularPlural + " in the brackets tournament");
+                StartBracketTournament(txtArray);
             }
             else if (numParticipants % 8 != 0)
             {
-                Console.WriteLine("We need " + participantsNeeded + " additional entries to create a brackets tournament");
+                Console.WriteLine("We need " + participantsNeeded + " additional entries to start a brackets tournament");
             }
+        }
+
+        public static void StartBracketTournament(string[] txtArray)
+        {
+            // Random Number Generator
+            var rand = new Random();
+
+            int numParticipants = txtArray.Length;
 
             // Initialize array for randomized bracket entries
             string[] bracketArray = new string[numParticipants];
 
             /*
-             * Available Bracket Positions
-             * This will be filled with unique random numbers
-             * These random numbers will be used to place each line of participants
-             * into the the bracket array in a random order
-             */
-            int[] bracketPositions = { -1, -1, -1, -1, -1, -1, -1, -1 };
+                * Available Bracket Positions
+                * This will be filled with unique random numbers
+                * These random numbers will be used to place each line of participants
+                * into the the bracket array in a random order
+                */
+            int[] bracketPositions = new int[numParticipants];
+            for (int i=0; i<numParticipants; i++)
+            {
+                bracketPositions[i] = -1;
+            }
+
             for (int i = 0; i < bracketPositions.Length; i++)
             {
-                int tempNum = rand.Next(8);
+                int tempNum = rand.Next(numParticipants);
                 while (bracketPositions.Contains(tempNum))
                 {
-                    tempNum = rand.Next(8);
+                    tempNum = rand.Next(numParticipants);
                 }
                 bracketPositions[i] = tempNum;
             }
 
             /*
-             * Copies participants from txtArray, ie from text file
-             * into a bracket array, in a random order
-             */
+                * Copies participants from txtArray, ie from text file
+                * into a bracket array, in a random order
+                */
             int iterator = 0;
             foreach (string line in txtArray)
             {
@@ -123,18 +137,18 @@ namespace BracketTournament
             Console.WriteLine();
 
             /*
-             * Create 2D array
-             * This will allow easier comparisons
-             */
+                * Create 2D array
+                * This will allow easier comparisons
+                */
 
             // create a list to store temp string data from file
             List<string> tempList = new List<string>();
 
             // places bracket array into a list
-            foreach(string line in bracketArray)
+            foreach (string line in bracketArray)
             {
                 string[] rowAsArray = line.Split(' ');
-                foreach(string item in rowAsArray)
+                foreach (string item in rowAsArray)
                 {
                     tempList.Add(item);
                 }
@@ -156,15 +170,15 @@ namespace BracketTournament
                 }
             }
 
-/*
- * ------ Determine Round Winners ----------------------------------------------
- *        Determining Winners should be refactored out as it's own method
- */
+            /*
+                * ------ Determine Round Winners ----------------------------------------------
+                *        Determining Winners should be refactored out as it's own method
+                */
 
             /*
-             * Determine Round One Winners
+                * Determine Round One Winners
 ---->        * Needs to be refactored <----
-             */
+                */
             Console.WriteLine("Round One Winners");
             string[,] roundTwo = new string[numRows / 2, numCols];
             int k = 0;
@@ -182,7 +196,7 @@ namespace BracketTournament
                 {
                     for (int j = 0; j < 4; j++)
                     {
-                        roundTwo[k, j] = bracket2DArray[i+1, j];
+                        roundTwo[k, j] = bracket2DArray[i + 1, j];
                     }
                 }
                 k++;
@@ -196,15 +210,15 @@ namespace BracketTournament
             Console.WriteLine();
 
             /*
-             * Determine Round Two Winners
-             */
+                * Determine Round Two Winners
+                */
             Console.WriteLine("Round Two Winners");
             string[,] roundThree = new string[numRows / 4, numCols];
             k = 0;
 
             for (int i = 0; i < 4 - 1; i += 2)
             {
-                if (Convert.ToInt32(roundTwo[i, 2]) > Convert.ToInt32(roundTwo[i+1, 2]))
+                if (Convert.ToInt32(roundTwo[i, 2]) > Convert.ToInt32(roundTwo[i + 1, 2]))
                 {
                     for (int j = 0; j < 4; j++)
                     {
@@ -215,7 +229,7 @@ namespace BracketTournament
                 {
                     for (int j = 0; j < 4; j++)
                     {
-                        roundThree[k, j] = roundTwo[i+1, j];
+                        roundThree[k, j] = roundTwo[i + 1, j];
                     }
                 }
                 k++;
@@ -229,8 +243,8 @@ namespace BracketTournament
             Console.WriteLine();
 
             /*
-             * Determine Winner & Runner Up
-             */
+                * Determine Winner & Runner Up
+                */
             Console.WriteLine("Final Results");
             if (Convert.ToInt32(roundThree[0, 3]) > Convert.ToInt32(roundThree[1, 3]))
             {
@@ -245,3 +259,4 @@ namespace BracketTournament
         }
     }
 }
+            
